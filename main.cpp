2be892cce1baf41c589297bb62a009e063b3c633
Bog3008 <106614305+Bog3008@users.noreply.github.com>
2023-06-12 11:55:49 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,35 +1,27 @@
-#include "opencv2/opencv.hpp"
+
 #include <iostream>
-#include "ObjectsDetector.hpp"
-#include <cmath>
-#include "Robot.hpp"
-#include "OrderQueue.hpp"
-#include <stdio.h>
-#include <Publisher.h>
 #include "TgBot.h"
-#include "Data.h"
-//cd E:\Clion Projects\DelBotSub\mosquitto\build\client\Debug
+#include "Delbot.hpp"
 
 void controller_start(){
-    try {
-        std::string tg_responce = tg_bot_run();
-        if (tg_responce != "start") {
-            //incorrect message handling;
-            std::cout << "unspecified message"<<std::endl;
-            return;
+    while(true) {
+        Delbot debo;
+        try {
+            std::string tg_responce = tg_bot_run();
+            if (tg_responce != "start") {
+                //incorrect message;
+                std::cout << "unspecified message" << std::endl;
+                return;
+            }
+            std::cout << tg_responce;
+            debo.run();
         }
-        std::cout << tg_responce;
-        RobotTest kos_bot(Data::cam);
-        kos_bot.do_clean();
-    }
-    catch(std::exception e){
-        std::cout << e.what();
+        catch (std::exception e) {
+            std::cout << e.what();
+        }
     }
 }
 
 int main() {
     controller_start();
-    /*
-     important info about host, port, topic, cam are in Data.h
-     */
 }
